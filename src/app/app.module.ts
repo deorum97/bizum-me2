e451e030d3a068f4e49104e61870b2ee7d5cfdea
deorum97 <jorge.r.9763@gmail.com>
2026-05-11 18:06:05 +0200

    principio de angular autentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,29 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'bizumme-52a11',
+        appId: '1:255288621040:web:34b7eff912592965d7cf35',
+        storageBucket: 'bizumme-52a11.firebasestorage.app',
+        apiKey: 'AIzaSyDr8oKJ3ZO_CFVsOVti4Me4NV4ozLIsnY8',
+        authDomain: 'bizumme-52a11.firebaseapp.com',
+        messagingSenderId: '255288621040',
+        measurementId: 'G-XBEDWQTZEZ',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
